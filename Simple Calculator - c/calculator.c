d2a6f8dcf38dc7c0a8c974963d25e48d7844e065
revandrazm <mahartarevandra@gmail.com>
2022-03-29 17:50:25 +0700

    COMMIT

---
+++ Simple Calculator - c/calculator.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main(void)
+{
+    double num1, num2, result;
+    char op;
+
+    while (true)
+    {
+        scanf("%lf %c %lf", &num1, &op, &num2);
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                break;
+            
+            case '-':
+                result = num1 - num2;
+                break;
+            
+            case 'x':
+                result = num1 * num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                result = num1 / num2;
+                break;
+
+            case '^':
+                result = pow(num1, num2);
+                break;
+
+            default:
+                continue;
+        }
+
+        printf("%lf\n", result);
+    }
+
+    return 0;
+}
