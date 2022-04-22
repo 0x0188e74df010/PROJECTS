ec41298d729eda6ff36479d7668010e4bce1e3f9
0x0188e74df010 <mahartarevandra@gmail.com>
2022-04-22 09:51:46 +0700

    commit

---
+++ other/define.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define a int
+#define b main
+#define c void
+#define d {
+#define e printf
+#define f ;
+#define g return
+#define h }
+
+a b(c)
+d
+	e("Hello, world\n")f
+
+	g 0 f
+h
